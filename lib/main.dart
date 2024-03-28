5f51fc1b0d262cc228c535684e3edd39d73fbb66
Jared <91357852+jad7qt@users.noreply.github.com>
2024-03-28 18:52:46 -0400

    Add google maps dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,6 +7,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
 
   // This widget is the root of your application.
   @override
